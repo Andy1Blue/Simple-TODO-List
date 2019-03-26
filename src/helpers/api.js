4b6e66d4bf223e7378f1c2368ad22472666a8aec
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-03-26 20:34:13 +0100

    Adding remove function

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -27,7 +27,14 @@ new Promise(
   (resolve, reject) => apiCall(url, 'POST', body, resolve, reject)
 )
 
-export const destroy = url =>
+export const remove = url =>
 new Promise(
-  (resolve, reject) =>
+  (resolve, reject) => {
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset:utf-8'
+      }
+    })
+  }
 )
