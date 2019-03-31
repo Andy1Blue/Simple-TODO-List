a53a68e28f636ed443bf05ef22ba165506242c3a
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-03-31 14:04:16 +0200

    Moving props inside component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDoList from './containers/ToDoList/ToDoList';
 import ToDoEditForm from './components/ToDoList/ToDoEditForm';
-import * as toDoItemsApiUrl from './helpers/toDoItemApi';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    tasks: null,
-    isLoading: true
-  }
-
-  componentDidMount = async () => {
-    const tasks = await toDoItemsApiUrl.getAll();
-    console.log(tasks);
-    this.setState({tasks, isLoading: false});
-  }
-
   render() {
-    const { isLoading, tasks } = this.state;
     return (
       <div className="App">
-      {isLoading && <div>Loading...</div>}
-      {!isLoading && tasks !== null &&
         <Router>
-          <Route exact path='/'>
-            <ToDoList title="TODO List" tasks={tasks}/>
-          </Route>
-
+          <Route exact path='/' component={ToDoList} />
           <Route path='/edit/:itemId' component={ToDoEditForm} />
         </Router>
-      }
       </div>
     );
   }
