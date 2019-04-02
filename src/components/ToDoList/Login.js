e095fb65d4216ca4cf96ba3d60d35d13f58c4eaf
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-04-02 20:25:18 +0200

    Cleaning Login component

--- src/components/ToDoList/Login.js
+++ src/components/ToDoList/Login.js
@@ -1,48 +1,24 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import { Redirect } from 'react-router-dom';
+import { CurrentUserConsumer } from '../../context/CurrentUser.context'
 
 class Login extends Component {
-  state = {
-    processing: false,
-    currentUser: null,
-    finished: false
-  }
-
-  fbLogin = async () => {
-    this.setState({processing: true});
-    window.FB.getLoginStatus(response => {
-      if(response.status !== 'connected') {
-       window.FB.login();
-      } else {
-        window.FB.api('/me', user => {
-          console.log(user);
-          sessionStorage.setItem('currentUser', user);
-          this.setState({finished: true, processing: false, currentUser: user});
-        })
-      }
-    });
-  }
-
   render() {
-    const { finished } = this.state;
     const { from } = this.props.location.state || { from: { pathname: '' } };
 
-    if (finished) {
-      return <Redirect to={from} />
-    }
-
     return (
-      <div>
-      {this.state.currentUser
-        ? <div>Hello, {this.state.currentUser.name}</div>
-        : <p>You must login to view page {from.pathname}.</p>
-      }
-      {this.state.processing
-          ? <div>Authenticating...</div>
-          : <button onClick={this.fbLogin}>Facebook login</button>
-      }
-      </div>
+      <CurrentUserConsumer>
+        {({user, login, processing}) => (
+          <div>
+            {user && <Redirect to={from} />}
+            <p>You must log in to view the page.</p>
+            {processing
+              ? <div>Authenticating</div>
+              : <button onClick={login}>Facebook login</button>}
+          </div>
+        )}
+      </CurrentUserConsumer>
     )
   }
 }
