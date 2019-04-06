5cea20098a23ab5311984c11c88a87de998a2431
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-04-06 17:48:24 +0200

    Adding config to a separate file

--- src/containers/ToDoList/Navbar.js
+++ src/containers/ToDoList/Navbar.js
@@ -4,7 +4,7 @@ import GoogleLogin from 'react-google-login';
 import '../../App.css';
 import { config } from '../../config';
 
-class Navbar extends Component { 
+class Navbar extends Component {
   render() {
     return (
       <CurrentUserConsumer>
@@ -12,12 +12,12 @@ class Navbar extends Component {
           <div>
             {user !== null
               ? <div><img src={user.profileObj.imageUrl} alt="user avatar" id="userImg"></img>Hello {user.profileObj.givenName}! <button onClick={logout}>Logout</button></div>
-              : 
-                <GoogleLogin
-              clientId={config.google}
-              buttonText="LOGIN WITH GOOGLE"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
+              :
+              <GoogleLogin
+                clientId={config.google}
+                buttonText="LOGIN WITH GOOGLE"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
               />
             }
           </div>
